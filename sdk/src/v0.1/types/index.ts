ab966938b2e9ae03dc5955e53d53e73398a83b60
jrooks7 <jeremy@metadao.fi>
2025-08-08 16:54:01 -0700

    fix: updated sdk again to ensure proper event transmission

--- sdk/src/v0.1/types/index.ts
+++ sdk/src/v0.1/types/index.ts
@@ -1,3 +1,5 @@
+import { TokenMigrator as TokenMigratorProgram } from "./token_migrator.js";
+
 export type { TokenMigrator } from "./token_migrator.js";
 
 // Account types
@@ -9,18 +11,14 @@ export type Vault = {
   bump: number[];
 };
 
-// Event types
-export type MigrateEvent = {
-  user: PublicKey;
-  mintFrom: PublicKey;
-  mintTo: PublicKey;
-  depositAmount: BN;
-  withdrawAmount: BN;
-};
+export type MigrateEvent = IdlEvents<TokenMigratorProgram>["migrateEvent"];
+
+export type TokenMigrationEvents = MigrateEvent;
 
 // Strategy enum type
 export type Strategy = { proRata: {} } | { fixed: { e: number } };
 
+import { IdlEvents } from "@coral-xyz/anchor";
 // Re-export PublicKey and BN for convenience
 import { PublicKey } from "@solana/web3.js";
 import BN from "bn.js";
